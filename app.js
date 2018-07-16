369adb40cd13391eda833dd734711750df87bc2d
panhaleap <leappanha1@yahoo.com>
2018-07-16 13:59:40 +0700

    can chat to everyone by using broadcast

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const io = require("socket.io")(http);
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-
 
 //  io.on('connection', function(socket){
 //     console.log('a user connected');
@@ -24,12 +23,29 @@ app.get('/', (req, res) => {
 //     });
 //   });
 
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//       console.log('message: ' + msg);
+//     });
+//   });
+
+// io.on('connection', function(socket){
+//     socket.broadcast.emit('hi');
+//   });
+  
+// io.on("connection", function(socket) {
+//   socket.on("chat message", function(msg) {
+//     io.emit("chat message", msg);
+//     console.log('message: ' + msg);
+//   });
+// });
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
+      io.emit('chat message', msg);
     });
   });
-  
-  http.listen(port, function(){
-    console.log(`Listening to port ${port}`);
-  });
+
+http.listen(port, function() {
+  console.log(`Listening to port ${port}`);
+});
