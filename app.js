fcb40778afa61f3a2d8758969f98a4c4b697fd88
panhaleap <leappanha1@yahoo.com>
2018-07-16 13:46:00 +0700

    printed message from client to console

--- app.js
+++ app.js
@@ -13,8 +13,21 @@ app.get('/', (req, res) => {
 });
 
 
- io.on('connection', function(socket){
-    console.log('a user connected');
+//  io.on('connection', function(socket){
+//     console.log('a user connected');
+//   });
+
+//   io.on('connection', function(socket){
+//     console.log('a user connected');
+//     socket.on('disconnect', function(){
+//       console.log('user disconnected');
+//     });
+//   });
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
   });
   
   http.listen(port, function(){
