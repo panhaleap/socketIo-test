f71571ee867ddef8f8b24888d88768d8abf422eb
panhaleap <leappanha1@yahoo.com>
2018-07-18 14:36:14 +0700

    show who chated the message, who is typing, input username before start the chat, if username already taken so need to input other username

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const app = expresses();
 const port = process.env.port || 8080;
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const users = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
+io.on("connection", function(socket) {
+  console.log("A user connected");
+  socket.on("setUsername", function(data) {
+    console.log(data);
 
-    socket.on('typing', function(handler){
-        socket.broadcast.emit('typing', handler);
-    });
+    if (users.indexOf(data) > -1) {
+      socket.emit(
+        "userExists",
+        data + " username is taken! Try some other username."
+      );
+    } else {
+      users.push(data);
+      socket.emit("userSet", { username: data });
+    }
   });
+
+  socket.on("msg", function(data) {
+    //Send message to everyone
+    io.sockets.emit("newmsg", data);
+  });
+
+  // User typing event
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data);
+});
+});
 
 http.listen(port, function() {
   console.log(`Listening to port ${port}`);
